5d6363dceef8399b80af760b4a7c99ecf15802c6
JP <jp@canyourigit.com>
2014-03-10 06:50:38 -0700

    update init

--- emacs/init.el
+++ emacs/init.el
@@ -367,6 +367,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(safe-local-variable-values (quote ((python-shell-interpreter-args . "/Users/john/Sites/reality-pickem/manage.py shell") (python-shell-interpreter . "python"))))
  '(send-mail-function (quote smtpmail-send-it))
  '(smtpmail-smtp-server "localhost")
  '(smtpmail-smtp-service 25))
